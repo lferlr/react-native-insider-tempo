bb4b8f94a104579e80c0ba1c1d0688bc15b8c9c3
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-03-23 00:46:37 -0300

    New key api[

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // https://api.hgbrasil.com/weather?key=e96474ca&lat=-23.682&lon=-46.875&user_ip=remote
 
-export const key = 'e96474ca';
+export const key = 'b7386740';
 
 const api = axios.create({
   baseURL: 'https://api.hgbrasil.com'
